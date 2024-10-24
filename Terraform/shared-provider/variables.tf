8dd9c7352a633057644228ce8db0a0f7404ee300
tsrlearning <info@tsrlearning.io>
2024-10-24 10:37:35 -0500

    create network

--- Terraform/shared-provider/variables.tf
+++ Terraform/shared-provider/variables.tf
@@ -7,5 +7,5 @@ variable "project_id" {
 variable "region" {
   description = "The region where GCP resources will be created (e.g., us-central1, europe-west1)."
   type        = string
-  default     = "us-south1"
+  default     = "us-central1"
 }
