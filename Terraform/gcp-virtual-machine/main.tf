2350782c1bda4a1332f6dad9edb84958a4a8d362
tsrlearning <info@tsrlearning.io>
2024-10-24 10:22:57 -0500

    create network

--- Terraform/gcp-virtual-machine/main.tf
+++ Terraform/gcp-virtual-machine/main.tf
@@ -5,9 +5,15 @@ module "shared_provider" {
 resource "google_compute_instance" "ubuntu_instance" {
   name                    = "gke-server1"
   machine_type            = "n1-standard-1"
-  zone                    = module.shared_provider.gcp_region
+  zone                    = "us-east1-a"
   tags                    = ["ssh-allowed"]
-  metadata_startup_script = "echo hi > /test.txt"
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    set -x
+    PUBLIC_IP=$(curl -s http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip -H "Metadata-Flavor: Google")
+    echo "The VM's public IP is: $PUBLIC_IP" > /public_ip.txt
+  EOT
 
   boot_disk {
     initialize_params {
